d90d48e4c5391de7e472b7000dba672b26c5561d
Maxemelianum <76476734+Maxemelianum@users.noreply.github.com>
2022-01-17 17:44:10 +0300

    new versions 17 jan 22

--- StoryHelper.py
+++ StoryHelper.py
@@ -17,6 +17,13 @@ sheet_9 = book['Лист9']
 sheet_10 = book['Лист10']
 sheet_11 = book['Лист11']
 
+COLUMN_THEME = 1
+COLUMN_MATERIAL = 3
+COLUMN_MATERIAL_LINK = 4
+COLUMN_TEST = 5
+COLUMN_TEST_LINK = 6
+
+
 
 def main():
     updater = Updater(token=TOKEN)  # объект, который ловит сообщения от Telegrama
@@ -26,7 +33,7 @@ def main():
     start_help = CommandHandler('help', do_help)
     start_handler = CommandHandler('start', do_start)
     learn_handler = CommandHandler('learn', do_learn)
-    cheker_handler = CommandHandler('chek', do_cheker)
+    cheker_handler = CommandHandler('check', do_cheker)
     changeclass_handler = MessageHandler(Filters.text, do_changeclass)
     echo = MessageHandler(Filters.all, do_echo)
 
@@ -42,8 +49,9 @@ def main():
     updater.idle()
 
 
-def do_echo(update, context):
+def do_echo(update: Update, context):
     update.message.reply_text('ЧаВо?')
+
 
 
 def do_start(update, context):
@@ -92,40 +100,60 @@ def do_changeclass(update: Update, context):
                               f'\nТесты - E3:{sheet_7["E3"].value} F3:{sheet_7["F3"].value}'
                               f'\nТесты - E4:{sheet_7["E4"].value} F4:{sheet_7["F4"].value}', reply_markup=ReplyKeyboardRemove())
 
-    if text == '7класс':
-        keyboard = [[sheet_7["A2"].value], ['тема2'], ['тема3'],
-                    ['тема4'], ['тема5'], ['тема6']]
-        reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
-        update.message.reply_text('Выбирите тему, которую хотите изучать.', reply_markup=reply_markup)
+    #if text == '7класс':
+     #   keyboard = [[sheet_7["A2"].value], ['тема2'], ['тема3'],
+     #               ['тема4'], ['тема5'], ['тема6']]
+   #     reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
+    #    update.message.reply_text('Выбирите тему, которую хотите изучать.', reply_markup=reply_markup)
 
-    if text == f'{sheet_7["A2"].value}':
-        keyboard = [[f'Материалы по "{sheet_7["A2"].value}"'], [f'Тесты по "{sheet_7["A2"].value}"']]
-        reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
-        update.message.reply_text('Хотите ознакомиться с материалами по теме или проверить свои знания?', reply_markup=reply_markup)
+   # if text == f'{sheet_7["A2"].value}':
+   #     keyboard = [[f'Материалы по "{sheet_7["A2"].value}"'], [f'Тесты по "{sheet_7["A2"].value}"']]
+   #     reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
+   #     update.message.reply_text('Хотите ознакомиться с материалами по теме или проверить свои знания?', reply_markup=reply_markup)
 
-    if text == f'Материалы по "{sheet_7["A2"].value}"':
-        keyboard = [[sheet_7["C2"].value], [sheet_7["C3"].value], [sheet_7["C4"].value]]
-        reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
-        update.message.reply_text('Выберите, то с чего хотите начать.', reply_markup=reply_markup)
+    #if text == f'Материалы по "{sheet_7["A2"].value}"':
+      #  keyboard = [[sheet_7["C2"].value], [sheet_7["C3"].value], [sheet_7["C4"].value]]
+      #  reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
+     #   update.message.reply_text('Выберите, то с чего хотите начать.', reply_markup=reply_markup)
 
-    elif text == f'{sheet_7["C2"].value}':
-        update.message.reply_text(f'{sheet_7["D2"].value}', reply_markup=ReplyKeyboardRemove())
-    elif text == f'{sheet_7["C3"].value}':
-        update.message.reply_text(f'{sheet_7["D3"].value}', reply_markup=ReplyKeyboardRemove())
-    elif text == f'{sheet_7["C4"].value}':
-        update.message.reply_text(f'{sheet_7["D4"].value}', reply_markup=ReplyKeyboardRemove())
+    #elif text == f'{sheet_7["C2"].value}':
+     #   update.message.reply_text(f'{sheet_7["D2"].value}', reply_markup=ReplyKeyboardRemove())
+    #elif text == f'{sheet_7["C3"].value}':
+     #   update.message.reply_text(f'{sheet_7["D3"].value}', reply_markup=ReplyKeyboardRemove())
+    #elif text == f'{sheet_7["C4"].value}':
+       # update.message.reply_text(f'{sheet_7["D4"].value}', reply_markup=ReplyKeyboardRemove())
 
-    if text == f'Тесты по "{sheet_7["A2"].value}"':
-        keyboard = [[sheet_7["E2"].value], [sheet_7["E3"].value], [sheet_7["E4"].value]]
-        reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
-        update.message.reply_text('Выбирите тест.', reply_markup=reply_markup)
+   # if text == f'Тесты по "{sheet_7["A2"].value}"':
+    #    keyboard = [[sheet_7["E2"].value], [sheet_7["E3"].value], [sheet_7["E4"].value]]
+     #   reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
+      #  update.message.reply_text('Выбирите тест.', reply_markup=reply_markup)
 
-    elif text == f'{sheet_7["E2"].value}':
-        update.message.reply_text(f'{sheet_7["F2"].value}', reply_markup=ReplyKeyboardRemove())
-    elif text == f'{sheet_7["E3"].value}':
-        update.message.reply_text(f'{sheet_7["F3"].value}', reply_markup=ReplyKeyboardRemove())
-    elif text == f'{sheet_7["E4"].value}':
-        update.message.reply_text(f'{sheet_7["F4"].value}', reply_markup=ReplyKeyboardRemove())
+  #  elif text == f'{sheet_7["E2"].value}':
+ #       update.message.reply_text(f'{sheet_7["F2"].value}', reply_markup=ReplyKeyboardRemove())
+ #   elif text == f'{sheet_7["E3"].value}':
+#        update.message.reply_text(f'{sheet_7["F3"].value}', reply_markup=ReplyKeyboardRemove())
+ #  elif text == f'{sheet_7["E4"].value}':
+#        update.message.reply_text(f'{sheet_7["F4"].value}', reply_markup=ReplyKeyboardRemove())
+
+    theme = []
+    answer = []
+    test = []
+    material = []
+
+    if text == '/check_value':
+
+        for line in range(2, sheet_7.max_row + 1):
+            theme.append(sheet_7.cell(row=line, column=COLUMN_THEME).value)
+        theme = set(theme)
+        keyboard = [theme]
+        key_board = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
+        update.message.reply_text('Посмотрите', reply_markup=key_board)
+        answer1 = update.message.text
+
+        for line in range(2, sheet_7.max_row + 1):
+            value = sheet_7.cell(row=line, column=COLUMN_THEME).value
+            if value == answer1:
+                answer1.append(sheet_7.cell(row=line, column=COLUMN_MATERIAL).value)
 
 
 main()
--- Value_checker.py
+++ Value_checker.py
@@ -2,7 +2,13 @@ from openpyxl import load_workbook
 
 
 book = load_workbook("DATAofStoryBotHelper.xlsx")
-page = book['Лист7']
+
+for page in book.sheetnames:
+    print(page)
+
+print('Введите название листа')
+
+page = book[input()]
 
 COLUMN_THEME = 1
 COLUMN_MATERIAL = 3
@@ -16,8 +22,8 @@ for line in range(2, page.max_row + 1):
     reply1.append(page.cell(row=line, column=COLUMN_THEME).value)
 
 reply1 = set(reply1)
-print('Выберите тему:')
-answer1 = input()
+print(reply1)
+answer1 = input('Введите тему:')
 answer2 = input('Материалы или тесты?')
 reply2 = []
 
@@ -25,3 +31,5 @@ for line in range(2, page.max_row + 1):
     value = page.cell(row=line, column=COLUMN_THEME).value
     if value == answer1:
         reply2.append(page.cell(row=line, column=COLUMN_MATERIAL).value)
+print(reply2)
+
